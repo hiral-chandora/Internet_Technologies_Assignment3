af8299a241019a0ae148be03270a8b4368765538
Hiral Chandora <hiral.chandora@gmail.com>
2015-07-31 18:08:52 +1000

    Create product2.php

---
+++ product2.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="description" content="Assignment1 - Internet Technologies" />
+		<meta name="keywords"    content="HTML, CSS" />
+		<meta name="author"      content="Hiral Chandora" />
+		<!-- title of page -->
+		<title>Flights to Cairo</title>
+		<!-- Reference to External CSS -->
+		<link href="styles/style.css" rel="stylesheet" type="text/css" media="all" />
+	</head>
+	<body>
+		<?php 
+			include('header.php'); 			// include php file of header and menu 
+		?>
+	<section>
+		
+		<article>			
+			<aside>
+				<h2>Flights to Cairo</h2>
+				<img src ="images/flight_to_cairo_inner_img.jpg" alt="Flights to Cairo" />
+			</aside>
+			<div class="content-wrapper">
+				<h2>Overview</h2>
+				
+				<p>Take a flight to Cairo with us, and explore Egypt’s largest city and the legacy of the pharaohs. Our Cairo flight will see you arrive relaxed and ready to experience this unique destination.</p>
+				<p>Deep-reclining comfort in First Class and Business Class, ice on-demand entertainment, and our delicious menu of inflight dining ensure your flight to Cairo is comfortable and pleasant.</p>
+				<h3>Travel to Cairo</h3>
+				<p>Our flights to Cairo take you to an extraordinary destination, where visitors from around the world arrive to see the Great Pyramids at Giza, the Sphinx and the countless artefacts displayed at the Egyptian Museum. Our flights to Cairo also bring you to an architectural wonderland where modern icons like the Cairo Tower are contrasted by the historic structures of Old Cairo.</p>
+				
+				<h2>Guide</h2>
+				<h3>About Cairo</h3>
+				<p>A city upon the Nile that has been inspiring humanity and drawing visitors since the dawn of recorded history, Cairo’s one-of-a-kind allure is as alive today as ever.</p>
+				<p>Cairo is a massive city with a long and well-preserved history of habitation that reaches back to the times of the Pharaohs, the Romans and the Greeks. The city almost resembles a pop-up book of architectural history with so many different period styles erected in a single space.</p>
+				<h3>Ancient Egypt</h3>
+				<p>Cairo is home to the Egyptian Museum, which contains an overwhelming amount of ancient artefacts and mummies, and is an important stop on any aspiring Egyptologist’s tour. However, Cairo itself is home to only a fraction of the standing monuments and burial sites of the pharaohs, which are well-scattered throughout Upper Egypt.</p>
+				<h3>Dining and Nightlife</h3>
+				<p>To experience authentic fare in Cairo, head to the ubiquitous street cafe to sample three popular Egyptian dishes: foul medammas, koshary and falafel. Vegetarian and filling, these grain and legume-based dishes will help get you through a busy day.</p>
+				<a href="product.php"> Back </a>
+			</div>
+			<div class="clear"></div>
+		</article>
+	</section>
+		<?php 
+			include('footer.php'); 		//Add Content of footer from php file
+		?>
+	</body>
+</html>
