7e1d90e024033b8028a33e0e4728cca1f58811db
Hiral Chandora <hiral.chandora@gmail.com>
2015-07-31 18:09:23 +1000

    Create product3.php

---
+++ product3.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="description" content="Assignment1 - Internet Technologies" />
+		<meta name="keywords"    content="HTML, CSS" />
+		<meta name="author"      content="Hiral Chandora" />
+		<!-- title of page -->
+		<title>Flights to Dubai</title>
+		<!-- Reference to External CSS -->
+		<link href="styles/style.css" rel="stylesheet" type="text/css" media="all" />
+	</head>
+	<body>
+		<?php 
+			include('header.php'); 			// include php file of header and menu 			
+		?>
+	<section>
+		
+		<article>			
+			<aside>
+				<h2>Flights to Dubai</h2>
+				<img src ="images/flight_to_dubai_inner_img.jpg" alt="Flights to Dubai" />
+			</aside>
+			<div class="content-wrapper">
+				<h2>Overview</h2>
+				
+				<p>All our flights to Dubai land at Dubai International Airport’s Terminal 3, our' dedicated terminal. This modern facility makes arriving from your Dubai flight as simple as possible, with a smooth and efficient path from arrival gate to baggage collection.</p>
+				<p>After arriving on a Dubai flight, there are a number of options to take you into the city itself.  The Dubai Metro has a station at Terminal 3, and is an efficient way of getting into town after a flight to Dubai. Taxis and buses are also on hand.</p>
+				<h2>Guide</h2>
+				<h3>About Dubai</h3>
+				<p>Few cities in the world have attracted as much attention as Dubai in recent years – but then, few cities have seen such a frenzied pace of evolution and development.</p>
+				<p>From its start as a small trading port, Dubai has successfully reinvented itself as a global hub for business and tourism, and has become, without hyperbole, one of the most remarkable cities in the world.</p>
+				<h3>Dubai sights</h3>
+				<p>For first-time visitors to Dubai, it’s the big things that often get the most attention, possibly because there are so many of them. Dubai is home to the tallest tower in the world (the Burj Khalifa), the largest man-made marina, and the largest man-made island (the Palm Jumeirah), just for starters.</p>
+				<p>The creek is Old Dubai’s geographic centre, and is still a working waterway with dozens of traditional dhow ships loading and unloading their cargoes on the creek side. Towards the mouth of the creek both shores are lined with Dubai’s souks (markets), offering a flavour of a time before malls.</p>
+				<h3>Nightlife</h3>
+				<p>Dubai’s many hotels offer a vast range of bars and nightspots, from places for a casual drink, to locations offering stunning vistas, to full-blown nightclubs.</p>
+				<p>Increasing numbers of international bands and DJs are adding Dubai to their touring schedules, and most months will see at least one big-name gig in town – check out local listings magazines for the most up-to-date information, and for tips on the city’s current hottest nightspots.</p>
+				<a href="product.php"> Back </a>
+			</div>
+			<div class="clear"></div>
+		</article>
+	</section>
+		<?php 
+			include('footer.php'); 		//Add Content of footer from php file
+		?>
+	</body>
+</html>
