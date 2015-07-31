98563721ebfa77db580c32838189472f684a8fce
Hiral Chandora <hiral.chandora@gmail.com>
2015-07-31 18:02:31 +1000

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>	
+	<p>e-mail : <a href="mailto:4960963@student.swin.edu.au">4960963@student.swin.edu.au</a></p>
+	<p>Terms and Conditions Reserved 2014</p>
+	<p>All content displayed on the page has been copied from <a href="http://www.emirates.com/" target="_blank">Emirates Airline Website</a></p>
+</footer>
