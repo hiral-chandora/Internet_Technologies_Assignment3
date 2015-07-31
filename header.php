2de61fe035f5a53dcc540fac66b6c1cd7fd748ca
Hiral Chandora <hiral.chandora@gmail.com>
2015-07-31 18:04:30 +1000

    Create header.php

---
+++ header.php
@@ -0,0 +1,31 @@
+<header>
+<div id="header-wrapper">
+	<a href="index.php"> <div id="logo"></div></a>
+	<div id="company-name">
+		<h1>Galippo Airlines</h1>
+	</div>
+</div>
+<nav>
+	<ul>
+		<li >        
+			<a href="index.php"> Home </a>
+		</li>
+		<li>
+			<a href="product.php"> Product </a>
+		</li>
+		<li>
+			<a href="about.php"> About </a>
+		</li>
+		<li>
+			<a href="customer.php"> Register </a>
+		</li>
+		<li>
+			<a href="customer_order_search.php"> Search</a>
+		</li>		
+		<li>
+			<a href="vendor_order.php"> Edit Orders</a>
+		</li>
+	</ul>
+</nav>
+</header>
+		
